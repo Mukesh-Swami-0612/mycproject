9cced11ca2bda401faf88f90a37fa68d01f415e3
Mukesh-Swami-0612 <mukesh704swami@gmail.com>
2025-02-15 00:42:13 +0530

    Mukesh

---
+++ Array10A.java
@@ -0,0 +1,13 @@
+public class Array10A {
+    public static void main(String args[]){
+        
+        int[] marks=new int[3];
+        marks[0]=10;
+        marks[1]=12;
+        marks[2]=13;
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
